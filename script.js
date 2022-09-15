df77496f26f85680a8f65cec3e2ed7b65407a541
AlvaroAbreu <alvaroabreu1509@gmail.com>
2022-09-14 22:50:53 -0300

    requisito 18 terminado

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 const btn = document.querySelector('#btn-login');
 const email = document.querySelector('#email');
 const senha = document.querySelector('#senha');
-console.log(email);
+// console.log(email);
 btn.addEventListener('click', () => {
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
     alert('Olá, Tryber!');
   } else { alert('Email ou senha inválidos.'); }
 });
+
+
+
+
+ const button = document.getElementById('submit-btn');
+ const checkbox1 = document.getElementById('agreement');
+
+ checkbox1.addEventListener('change', () => {
+     if (checkbox1.checked === true){
+      button.removeAttribute('disabled');
+     }
+     else {
+      button.setAttribute('disabled', 'disabled');
+     }
+ });
+
+ 
+
+
+
